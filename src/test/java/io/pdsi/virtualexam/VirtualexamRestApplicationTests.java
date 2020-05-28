6944ad62d88ff96f02f7ae287912cdf48319ae20
adrian61 <ad.kowalskI@gmail.com>
2020-05-28 21:45:52 +0200

    Initial commit

---
+++ src/test/java/io/pdsi/virtualexam/VirtualexamRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pdsi.virtualexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualexamRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
